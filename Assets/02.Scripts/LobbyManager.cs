f58ec2afdd237ac2763bb6d669b0e2e7be9f66ea
thlee <sheena1131@kakao.com>
2024-04-04 12:59:08 +0900

    [이태환] 로비씬 연출 완성

--- Assets/02.Scripts/LobbyManager.cs
+++ Assets/02.Scripts/LobbyManager.cs
@@ -11,11 +11,10 @@ public class LobbyManager : MonoBehaviour
 
     public GameObject Plasma;
     public GameObject tower;
-    public GameObject Trash;
-
+    public GameObject[] trashs;
 
     public GameObject towerRuins;
-    private float explosionForce = 1500f;
+    private float explosionForce = 15000f;
     private float explosionRadius = 15f; 
     void Awake()
     {
@@ -38,34 +37,36 @@ public class LobbyManager : MonoBehaviour
     
         yield return new WaitForSeconds(0.5f);
         ExplodeTowerRuins();
+        yield return new WaitForSeconds(0.5f);
 
-        yield return new WaitForSeconds(1.5f);
-        tower.transform.DOMoveY(-500f, 10f).OnComplete(() =>
+        tower.transform.DOMoveY(-200f, 10f).OnComplete(() =>
         {
             tower.SetActive(false);
         });
+        yield return new WaitForSeconds(1f);
+   
 
         Plasma.SetActive(false);
-        yield return new WaitForSeconds(1.5f);
-
-   
-        Trash.SetActive(false);
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(5f);
+        foreach (GameObject trash in trashs) { 
+        trash.SetActive(false);
+        }
 
     }
     void ExplodeTowerRuins()
     {
         towerRuins.SetActive(true);
-
+  
         // 타워 잔해에 있는 모든 Rigidbody 컴포넌트에 폭발력을 적용
         foreach (Transform child in tower.transform)
         {
             Rigidbody rb = child.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                rb.AddExplosionForce(explosionForce, towerRuins.transform.position, explosionRadius);
+                rb.AddExplosionForce(explosionForce, tower.transform.position, explosionRadius);
             }
         }
+
     }
 
 
